617d4f8ca78c1c7621a4b351e2bc217ff4e5fa82
Marcus Fulton <marcusfulton11@yahoo.com>
2021-06-21 21:16:14 +0100

    comments added and code cleaned up with better Variable names.

--- horiscope project.js
+++ horiscope project.js
@@ -1,18 +1,18 @@
-const randomNumberGenerator = (number) => {
-    let randomNum = Math.floor(Math.random() * number);
+const randomNumberGenerator = (number) => { // method with parameter
+    let randomNum = Math.floor(Math.random() * number); // creates random number
     return randomNum;
 
 };
 
 // creates arrays with info about starSigns and such. 
-const astrologyInformation = {
+const astrologyInformation = { // creates method with 3 parameters
     signs: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Capricorn', 'Aquarius', 'Pisces'],
     fortune: ['you will have good luck!', 'your luck will be ok', 'you will have very good luck!', 'you will have bad luck!', 'oh no, very bad luck!'],
-    adivice: ['Take time to know yourself', 'Be patient', 'In order to get, you have to give', 'do your best',
+    advice: ['Take time to know yourself', 'Be patient', 'In order to get, you have to give', 'do your best',
               'Don\'t be afraid of being afraid','Don\'t make decisions when you are angry', 'Don\'t worry what other people think',
                'Do what is right, not what is easy', ' Believe in yourself']
 };
-const starSigns = {
+const starSigns = { // creates method with a few param
     Aries: 'March 21 – April 20',
     Taurus: 'April 21 – May 20',
     Gemini: 'May 21 – June 21',
@@ -27,22 +27,24 @@ const starSigns = {
  };
 
  // To store random message in 'messageData'
- let messageData = [];
- for (let prop in astrologyInformation) {
-     let keyValue = astrologyInformation[prop];
-     let randomIndex = randomNumberGenerator(keyValue.length);
-     messageData.push(keyValue[randomIndex]);
+ let messageData = []; // empty array
+ for (let key in astrologyInformation) {
+     let keyValue = astrologyInformation[key]; // creates variable that is equal to the key value in astrologyInformation
+     let randomIndex = randomNumberGenerator(keyValue.length); // creates variable that is equal to a random value on the keyValue
+     messageData.push(keyValue[randomIndex]); // pushes the random keyValue to the messageData array
  };
- 
+
  // Main Function. this will log the Message on to the screen 
- const astrologyMessage = (Array) => {
-    let messageToShow = [];
-    let dateOfBirth = Array[0];
-    messageToShow.push(`My guess is that your star sign is - ${Array[0]} So, You were born between ${starSigns[dateOfBirth]}`);
-    messageToShow.push(`Your luck-metre says - ${Array[1]}`);
-    messageToShow.push(`My Advice to you is - "${Array[2]}"`);
-    const theMessageString = messageToShow.join('\n');
-    console.log(theMessageString);
+ const astrologyMessage = (Array) => { // function called astrologyMessage with a param called Array
+    let messageToShow = []; // empty array
+    let randomSign = Array[0]; // var called randomSign which takes gives a random sign from astrologyInformation[signs]
+    let randomFortune = Array[1]; // same but with fortune
+    let randomAdvice = Array[2];  // same but with advice
+    messageToShow.push(`My guess is that your star sign is - ${randomSign} So, You were born between ${starSigns[randomSign]}`); // adds to empty 'messageToShow' array
+    messageToShow.push(`Your luck-metre says - ${randomFortune}`); // adds to empty 'messageToShow' array
+    messageToShow.push(`My Advice to you is - "${randomAdvice}"`); // adds to empty 'messageToShow' array
+    const theMessageString = messageToShow.join('\n'); // joins the messages together
+    console.log(theMessageString); // prints to console.
 };
 
-astrologyMessage(messageData);
+astrologyMessage(messageData); // returns the random message.
