2351babba554b666b76f3998292d83000c6e653f
Marcus Fulton <marcusfulton11@yahoo.com>
2021-06-21 19:47:31 +0100

    variables to store astroInfo and starSign DATA

--- horiscope project.js
+++ horiscope project.js
@@ -2,3 +2,25 @@ const randomNumberGenerator = (number) => {
     let randomNum = Math.floor(Math.random() * number);
     return randomNum;
 };
+
+// Wisdom variable
+const astrologyInformation = {
+    signs: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Capricorn', 'Aquarius', 'Pisces'],
+    fortune: ['you will have good luck!', 'your luck will be ok', 'you will have very good luck!', 'you will have bad luck!', 'oh no, very bad luck!'],
+    adivice: ['Take time to know yourself', 'Be patient', 'In order to get, you have to give', 'do your best',
+              'Don\'t be afraid of being afraid','Don\'t make decisions when you are angry', 'Don\'t worry what other people think',
+               'Do what is right, not what is easy', ' Believe in yourself']
+};
+const timesOfBorn = {
+    Aries: 'March 21 – April 20',
+    Taurus: 'April 21 – May 20',
+    Gemini: 'May 21 – June 21',
+    Cancer: 'June 22 – July 22',
+    Leo: 'July 23 – August 22',
+    Virgo: 'August 23 – September 21',
+    Libra: 'September 22 – October 22',
+    Scorpio: 'November 23 – December 21',
+    Capricorn: 'December 22 – January 20',
+    Aquarius: 'January 21 – February 19',
+    Pisces: 'February 20 – March 20',
+ };
